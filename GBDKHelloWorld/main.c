743d2f4e3c4af0f7e3cc434d4803989851cd0856
M. O. Marmalade <xephyrpanda@yahoo.com>
2020-02-07 21:20:47 -0800

    learning sprites

--- GBDKHelloWorld/main.c
+++ GBDKHelloWorld/main.c
@@ -1,9 +1,38 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "wiggleSprite.c"
 
 int main() {
-	printf("HELLO WORLD!\n");
+	//printf("HELLO LITTLE WIGGLER\n");
 	//UINT16 d = 3;
 	//delay(d);
-	printf("New Print!");
+	//printf("New Print!");
+	UINT8 counter = 0;
+	UINT8 currentSpriteIndex = 0;
+	set_sprite_data(0, 3, wiggler);	
+	set_sprite_tile(0, 0);
+	move_sprite(0, 88, 78);
+
+	SHOW_SPRITES;
+
+	while (1) {
+		if (counter == 0) {
+			currentSpriteIndex = 0;
+			counter++;
+		}
+		else if (counter == 1) {
+			currentSpriteIndex = 1;
+			counter++;
+		}
+		else if (counter == 2) {
+			currentSpriteIndex = 0;
+			counter++;
+		}
+		else {
+			currentSpriteIndex = 2;
+			counter = 0;
+		}
+		set_sprite_tile(0, currentSpriteIndex);
+		delay(200);
+	}
 }
