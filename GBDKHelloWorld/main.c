65028cfeee32a976083125c957c66c7d1ca81c2e
M. O. Marmalade <xephyrpanda@yahoo.com>
2020-02-17 20:50:35 -0800

    allow player to scroll sprite with the D-Pad

--- GBDKHelloWorld/main.c
+++ GBDKHelloWorld/main.c
@@ -9,6 +9,7 @@ int main() {
 	//printf("New Print!");
 	UINT8 counter = 0;
 	UINT8 currentSpriteIndex = 0;
+	UINT8 whileCounter1 = 0;
 	set_sprite_data(0, 3, wiggler);	
 	set_sprite_tile(0, 0);
 	move_sprite(0, 88, 78);
@@ -16,6 +17,41 @@ int main() {
 	SHOW_SPRITES;
 
 	while (1) {
+		while (whileCounter1 != 20){
+			switch (joypad()) {
+			case J_UP | J_LEFT:
+				scroll_sprite(0, -1, -1);
+				break;
+			case J_UP:
+				scroll_sprite(0, 0, -1);
+				break;	
+			case J_UP | J_RIGHT:
+				scroll_sprite(0, 1, -1);
+				break;
+			case J_RIGHT:
+				scroll_sprite(0, 1, 0);
+				break;
+			case J_DOWN | J_RIGHT:
+				scroll_sprite(0, 1, 1);
+				break;
+			case J_DOWN:
+				scroll_sprite(0, 0, 1);
+				break;
+			case J_DOWN | J_LEFT:
+				scroll_sprite(0, -1, 1);
+				break;
+			case J_LEFT:
+				scroll_sprite(0, -1, 0);
+				break;			
+			}
+
+			set_sprite_tile(0, currentSpriteIndex);
+			delay(10);
+			whileCounter1++;
+		}
+
+		whileCounter1 = 0;
+
 		if (counter == 0) {
 			currentSpriteIndex = 0;
 			counter++;
@@ -32,7 +68,7 @@ int main() {
 			currentSpriteIndex = 2;
 			counter = 0;
 		}
+
 		set_sprite_tile(0, currentSpriteIndex);
-		delay(200);
 	}
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+Exploring the GBDK with Visual Studio
